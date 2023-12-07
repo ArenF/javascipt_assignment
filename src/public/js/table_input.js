7a63d4dcb61023a9fa938b5992a3f45b6057431f
Hiru512 <hyeonmini512@gmail.com>
2023-12-08 01:13:26 +0900

    localStorage에다 성적테이블 값 저장

--- src/public/js/table_input.js
+++ src/public/js/table_input.js
@@ -1,13 +1,32 @@
 
 const button = document.getElementById("submit_button");
 
+const formTable = document.querySelector("#input_table");
 const nameInput = document.getElementsByName("name")[0];
 const codeInput = document.getElementsByName("student_code")[0];
 const korInput = document.getElementsByName("sub_korean")[0];
 const engInput = document.getElementsByName("sub_english")[0];
-const matInput = document.getElementsByName("sub_math")[0];
+const mathInput = document.getElementsByName("sub_math")[0];
+
+const HIDDEN_CLASSNAME = "hidden";
 
 button.addEventListener("click", (event) => {
     event.preventDefault();
     console.log("Hello world!");
+
+    formTable.classList.add(HIDDEN_CLASSNAME);
+
+    const stdName = nameInput.value;
+    const stdCode = codeInput.value;
+    const scrKor = korInput.value;
+    const scrEng = engInput.value;
+    const scrMath = mathInput.value;
+
+    localStorage.setItem("stdName", stdName);
+    localStorage.setItem("stdCode", stdCode);
+    localStorage.setItem("scrKor", scrKor);
+    localStorage.setItem("scrEng", scrEng);
+    localStorage.setItem("scrMath", scrMath);
 });
+
+button.addEventListener("submit", button);
