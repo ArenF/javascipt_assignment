56af611be2e539b6b1495cb98c0eb6ac55b11a74
rudwns123 <kyungjun0109@gmail.com>
2023-12-12 23:31:15 +0900

    nav 최종완료

--- src/router/exchange.js
+++ src/router/exchange.js
@@ -4,7 +4,9 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
     // res.sendFile(path.join(__dirname, "../views/exchange.html"));
-    res.render("exchange");
+    res.render("exchange", {
+        login: !req.session.loggedIn
+    });
 });
 
 module.exports = router;
